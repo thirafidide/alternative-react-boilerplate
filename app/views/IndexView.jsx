fcefda6afbd5e1d8082792850895691f4589e570
Thirafi Dide <jurnaldide@gmail.com>
2016-02-11 13:10:31 +0700

    add links to serve as example of react-router

--- app/views/IndexView.jsx
+++ app/views/IndexView.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import TimeElapsed from '../components/TimeElapsed';
+import { Link } from 'react-router';
 
 export default class IndexView extends React.Component {
   render() {
     return (
       <div>
         <h1>Alt(ernative) React Boilerplate</h1>
-        <p>A simple boilerplate inspired by <a href="http://survivejs.com">SurviveJS</a></p>
-        <p>A simple example using react and alt to show elapsed time : <TimeElapsed /> second(s) </p>
+        <ul>
+          <li><Link to="/example">Example use of Alt</Link></li>
+          <li><Link to="/about">About Alt(ernative) React Boilerplate</Link></li>
+        </ul>
       </div>
     );
   }
