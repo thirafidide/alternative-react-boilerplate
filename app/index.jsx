b1f5b20d29a8159c8d96a55ee6ffe75c34ffb6a9
Thirafi Dide <jurnaldide@gmail.com>
2016-02-12 20:51:22 +0700

    use browser history for cleaner url

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,13 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import IndexView from './views/IndexView';
 import AltExampleView from './views/AltExampleView';
 import AboutView from './views/AboutView';
 
 const appRouter = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={IndexView} />
     <Route path="/example" component={AltExampleView} />
     <Route path="/about" component={AboutView} />
